726a33cf51544119b5e0083b18388012ef3c38d9
Dung Dau <dudau0924222@gmail.com>
2022-04-24 16:05:18 -0400

    set up server

---
+++ backend/data.js
@@ -0,0 +1,42 @@
+const data = {
+    products: [
+        {
+            name: 'Nike Slim shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quailty shirt',
+        },
+        {
+            name: 'Nike Slim pants',
+            slug: 'nike-slim-pants',
+            category: 'Pants',
+            image: '/images/p3.jpg',
+            price: 25,
+            countInStock: 15,
+            brand: 'Nike',
+            rating: 4.4,
+            numReviews: 14,
+            description: 'high quailty pants',
+        },
+        {
+            name: 'Adidas Fit pants',
+            slug: 'adidas-slim-pants',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 65,
+            countInStock: 5,
+            brand: 'Puma',
+            rating: 4.0,
+            numReviews: 22,
+            description: 'high quailty pants',
+        },
+    ],
+};
+
+export default data;
