611d6f4a9e7b43ec1a41268783cbb957b0b52285
Dung Dau <dudau0924222@gmail.com>
2022-04-24 18:53:59 -0400

    implement data fetching

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data';
+import axios from 'axios';
+import logger from 'use-reducer-logger';
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'FETCH_REQUEST':
+      return {...state, loading: true}
+    case 'FETCH_SUCCESS':
+      return {...state, products: action.payload, loading: false}
+    case 'FETCH_FAIL':
+      return {...state, loading: false, error: action.payload};
+    default:
+      return state;
+  }
+}
 
 function HomeScreen() {
+  const [{loading, error, products}, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true, 
+    error: '',
+  })
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({type: 'FETCH_REQUEST'});
+      try {
+        const result = await axios.get('/api/products');
+        dispatch({type: 'FETCH_SUCCESS', payload: result.data});
+      } catch(err) {
+        dispatch({type: 'FETCH_FAIL', payload: err.message});
+      }
+    }
+    fetchData();
+  }, [])
   return (
     <div>
         <h1>Featured Products</h1>
         <div className="products">
-          {data.products.map((product) => (
+          {loading ? (
+            <div>Loading...</div>
+          ) : error ? (
+            <div>{error}</div>
+          ) : (
+          products.map((product) => (
             <div className="product" key={product.slug}>
               <Link to={`/product/${product.slug}`}>
                 <img src={product.image} alt={product.name} />
@@ -20,6 +56,7 @@ function HomeScreen() {
                 <button>Add to cart</button>
               </div>
             </div>
+          )
           ))}
         </div>
     </div>
