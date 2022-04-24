08221645d1c13e31e08ee1d461f305847922ad0d
Dung Dau <dudau0924222@gmail.com>
2022-04-24 15:41:39 -0400

    add basic routes and move all frontend folders to a single folder

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,34 +1,29 @@
-import data from "./data";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import data from '../data';
 
-function App() {
+function HomeScreen() {
   return (
     <div>
-      <header>
-        <a href="/">Ecommerce Store</a>
-
-      </header>
-      <main>
         <h1>Featured Products</h1>
         <div className="products">
           {data.products.map((product) => (
             <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
+              <Link to={`/product/${product.slug}`}>
                 <img src={product.image} alt={product.name} />
-              </a>
+              </Link>
               <div className="product-info">
-                <a href={`/product/${product.slug}`}>
+                <Link to={`/product/${product.slug}`}>
                   <p>{product.name}</p>
-                </a>
+                </Link>
                 <p><strong>${product.price}</strong></p>
                 <button>Add to cart</button>
               </div>
             </div>
           ))}
         </div>
-      </main>
-      
     </div>
-  );
+  )
 }
 
-export default App;
+export default HomeScreen
