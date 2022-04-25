7006cce679b0988c83ec24924ed8f54cbd691a71
Dung Dau <dudau0924222@gmail.com>
2022-04-25 13:40:36 -0400

    utilize bootstrap to change how products are rendered on the homepage and add ratings to the product component

---
+++ frontend/src/components/Rating.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+function Rating({rating, numReviews}) {
+  return (
+    <div className='rating'>
+        <span>
+            <i className={
+                rating >= 1 ? 
+                'fas fa-star' :
+                rating >= 0.5 ? 
+                'fas fa-star-half-alt' : 
+                'far fa-star'}/>
+        </span>
+        <span>
+            <i className={
+                rating >= 2 ? 
+                'fas fa-star' :
+                rating >= 0.5 ? 
+                'fas fa-star-half-alt' : 
+                'far fa-star'}/>
+        </span>
+        <span>
+            <i className={
+                rating >= 3 ? 
+                'fas fa-star' :
+                rating >= 0.5 ? 
+                'fas fa-star-half-alt' : 
+                'far fa-star'}/>
+        </span>
+        <span>
+            <i className={
+                rating >= 4 ? 
+                'fas fa-star' :
+                rating >= 0.5 ? 
+                'fas fa-star-half-alt' : 
+                'far fa-star'}/>
+        </span>
+        <span>
+            <i className={
+                rating >= 5 ? 
+                'fas fa-star' :
+                rating >= 0.5 ? 
+                'fas fa-star-half-alt' : 
+                'far fa-star'}/>
+        </span>
+        <span>{numReviews} reviews</span>
+    </div>
+  )
+}
+
+export default Rating
