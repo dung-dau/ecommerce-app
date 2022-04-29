043d77aab4635183e44357f84226d1e6850865cb
Dung Dau <dudau0924222@gmail.com>
2022-04-29 18:38:12 -0400

    implement payment by PayPal

--- backend/server.js
+++ backend/server.js
@@ -16,14 +16,19 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-
 // the use method is used to configure middleware that is 
 // used by the routes 
 // middleware are functions that have access to the req and
 // res objects and the next middleware function in the 
 // application’s request-response cycle
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get('/api/keys/paypal', (req, res) => {
+  // sb means sandbox
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
+
 
 // adds the middleware from ./routes/seedRoutes.js and
 // setting /api/seed as the first part of the route
